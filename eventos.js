114f197b2f67722c78b9e0420351299899056ee7
IvoPaitano <paitanoivo@hotmail.com>
2020-02-11 19:49:42 -0300

    Javascript

---
+++ eventos.js
@@ -0,0 +1,75 @@
+//                         BEEPER
+
+// var boton = document.querySelector("button")
+
+// boton.addEventListener("click", seEjecutaEnEvento)
+
+// var seEjecutaEnEvento = function(){
+//     document.querySelector("body").insertAdjacentHTML("beforeend","<p>BEEP</p>")
+//     document.querySelector("body").classList.toggle("color")
+// }
+
+//                         SHOW ME THE MONEY
+
+// document.querySelector("#mostrar1").addEventListener("click",function() {
+//     document.querySelector("#uno").classList.toggle("oculto")
+// })
+
+// document.querySelector("#mostrar2").addEventListener("click",function() {
+//     document.querySelector("#dos").classList.toggle("oculto")
+// })
+
+// document.querySelector("#mostrar3").addEventListener("click",function() {
+//     document.querySelector("#tres").classList.toggle("oculto")
+// })
+// document.querySelector("#uno").addEventListener("click",function name(params) {
+//     this.classList.add("oculto")
+// })
+
+// document.querySelector("#dos").addEventListener("click",function() {
+//     this.classList.add("oculto")
+// })
+
+// document.querySelector("#tres").addEventListener("click",function() {
+//     this.classList.add("oculto")
+// })
+
+
+//                         MOUSE TRACKER
+
+// var body = document.querySelector("body")
+// var bobEsponja = document.querySelector("img")
+
+// body.addEventListener("mousemove",function (infoMouse) {
+//     var y = infoMouse.clientY
+//     bobEsponja.style.top = y + "px"
+//     // document.querySelector("body").style.cursor = "none" CURSOR INVISIBLE CON
+// })
+
+
+//                         MAGIC DIV
+
+// document.querySelector(".magic").addEventListener("mouseenter",function() {
+//     document.querySelector("img").classList.toggle("invisible")
+// })
+
+//                         SELECCIONAR BACKGROUND           
+
+// var color = []
+
+// document.querySelector("input").addEventListener("keydown",function(teclado) {
+//     var tecla = String.fromCharCode(teclado.keyCode)
+//     color = color + tecla  
+//     console.log(color);
+// })
+
+// document.querySelector("body").addEventListener("click",function () {
+//     this.style.backgroundColor = color
+// })
+
+// document.querySelector("button").addEventListener("click",function() {
+//     color = []
+//     document.querySelector("body").style.backgroundColor = "white"
+// })
+
+
